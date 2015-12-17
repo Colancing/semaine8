6259e0dbbc31bfcfd35e527068e41a7d82c9e1b5
titzelle <pseudoups@hotmail.fr>
2015-12-17 22:18:47 +0100

    formualaire ok

--- post_contact.php
+++ post_contact.php
@@ -5,6 +5,34 @@
  * Date: 15/12/2015
  * Time: 22:14
  */
-var_dump($_POST);
-var_dump($_GET);
-echo $_POST['name'];
+$name    = $_POST['name'];
+$message = $_POST['message'];
+$mail    = $_POST['mail'];
+$headers = "from: " . $mail;
+
+$errors = [ ];
+
+if ( ! isset ( $message ) || $message == "" ) {
+	$errors['message'] = "Il n'y a aucun message";
+}
+if ( ! isset ( $name ) || $name == "" ) {
+	$errors['name'] = "Merci d'indiquer votre nom.";
+}
+if ( ! isset ( $mail ) || $mail == "" ) {
+	$errors['mail'] = "Merci d'indiquer votre adresse mail.";
+}
+
+if ( ! empty ( $errors ) ) {
+	session_start();
+	$_SESSION['errors']=$errors;
+	header( 'location:http://localhost/Simplon/semaine8/cv_porfolioo.php' );
+} else {
+	echo "Votre message a bien été envoyé";
+	mail( "p@hotmail.fr", "Formulaire de Contact Cv" . $headers, $message, $headers );
+}
+
+var_dump( $errors );
+die();
+
+
+
